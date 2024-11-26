e667a48afeea6714a9492486242c9f1c34df1a18
Volodymyr Matsiuk <matsiuk.dev@gmail.com>
2024-11-26 17:14:09 +0100

    Add Mapstruct and using DTO for User

---
+++ src/main/java/org/mvoks/datatransfer/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package org.mvoks.datatransfer.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mvoks.datatransfer.dto.UserDto;
+import org.mvoks.datatransfer.entity.user.User;
+
+@Mapper(componentModel = "jakarta")
+public interface UserMapper extends Mappable<User, UserDto> {
+
+    @Mapping(target = "password", qualifiedByName = "obfuscation-password")
+    @Override
+    UserDto toDto(User user);
+
+    @Named("obfuscation-password")
+    default String obfuscationPassword(String password) {
+        return "***";
+    }
+}
